1bc5e99759495224737cab9f56ec0de66770067e
alianhakim8 <alianhakim9@gmail.com>
2021-09-28 11:05:08 +0700

    pertemuan 3 play with jupyter notebook

--- pertemuan2/pertemuan2.py
+++ pertemuan2/pertemuan2.py
@@ -1,8 +1,13 @@
 # import library opencv
 import cv2
 
+print("Program dimulai")
+
 # load image
-img = cv2.imread('assets/gambar-1.png', 0)
+img = cv2.imread('assets/gambar.png', 1)
+# print(img.shape)
+# print(img[100, 100, 0])
+print(img)
 
 # mengatur ukuran window supaya yang tampil gak kebesaran
 imS = cv2.resize(img, (960, 540))
@@ -15,3 +20,5 @@ cv2.waitKey(0)
 
 # hapus semua window yang ada
 cv2.destroyAllWindows()
+
+print("Program selesai")
